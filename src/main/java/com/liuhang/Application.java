bd02e5d980d3f9e8ebbae94aa0834ee7607267ea
liuhang <liuhang93@foxmail.com>
2017-07-19 18:38:16 +0800

    Spring MVC config by Spring boot

--- src/main/java/com/liuhang/Application.java
+++ src/main/java/com/liuhang/Application.java
@@ -2,12 +2,19 @@ package com.liuhang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by liuhang on 2017/7/19.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
